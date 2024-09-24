2a4635721f9f13a8c56a0e99e548510bd5300bab
Fabio Ferro Junior <fabioferrojunior2@gmail.com>
2024-09-23 22:13:19 -0300

    First PlayTest
    Preps for 26/09 playtest

--- Assets/_MAIN/Scripts/Core/Characters/Types/CharacterSprite.cs
+++ Assets/_MAIN/Scripts/Core/Characters/Types/CharacterSprite.cs
@@ -1,12 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 namespace _MAIN.Scripts.Core.Characters.Types
 {
     public class CharacterSprite : Character
     {
-        public CharacterSprite(string name, CharacterConfigData config) : base(name, config)
+        private CanvasGroup RootCg => Root.GetComponent<CanvasGroup>();
+
+        public CharacterSprite(string name, CharacterConfigData config, GameObject prefab) : base(name, config, prefab)
         {
+            RootCg.alpha = 0;
+            
             Debug.Log($"Created Sprite Character: '{name}'");
+        }
+
+        public override IEnumerator ShowingOrHiding(bool show)
+        {
+            float targetAlpha = show ? 1f : 0;
+            CanvasGroup self = RootCg;
+            
+            while (self.alpha != targetAlpha)
+            {
+                self.alpha = Mathf.MoveTowards(self.alpha, targetAlpha, 3f * Time.deltaTime);
+                yield return null;
+            }
+
+            CoRevealing = null;
+            CoHiding = null;
         }
     }
 }
